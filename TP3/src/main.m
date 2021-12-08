13f8431644b5db37db110783cd3f7d4956e9e2be
Pedro Rodrigues <pedromiguelrodrigues2000@gmail.com>
2021-12-08 21:11:18 +0000

    Adding image thresholder

--- TP3/src/main.m
+++ TP3/src/main.m
@@ -1,3 +1,10 @@
 function main()
-    disp("TP3")
+    preprocess("hello", true);
+end
+
+
+function preprocess(root, save)
+    % preprocess images and save them for the next time avoiding spending
+    % time in preprocessing
+    disp(save);
 end
