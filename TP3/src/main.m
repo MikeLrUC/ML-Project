c60714b3a8f1c0ba2c0a4ee4d9176302e4a3a3c0
MikeLrUC <miguelrabuge12@gmail.com>
2021-12-09 00:24:30 +0000

    Added: TP3 Pre-Processing

--- TP3/src/main.m
+++ TP3/src/main.m
@@ -1,10 +1,88 @@
 function main()
-    preprocess("hello", true);
+    root = "data";
+    processed = "processed";
+    method = "rgb_average";
+    ratio = 0.7;
+    try
+        disp("Trying to Load Processed Data")
+        % Load Saved Processed Data
+        X_train = load(fullfile(root, processed, "X_train"));
+        y_train = load(fullfile(root, processed, "y_train"));
+        X_test = load(fullfile(root, processed, "X_test"));
+        y_test = load(fullfile(root, processed, "y_test"));
+    catch
+        disp("Not Found: Processing data...")
+        % Load and Transform Data
+        [data, labels] = preprocess(root, method);
+
+        % Split Dataset (Train/Test)
+        [X_train, y_train, X_test, y_test] = split_dataset(data, labels, ratio);
+        
+        if ~exist(fullfile(root, processed), "dir")
+            mkdir(fullfile(root, processed));
+        end
+
+        % Save Network
+        save(fullfile(root, processed, "X_train"), "X_train");
+        save(fullfile(root, processed, "y_train"), "y_train");
+        save(fullfile(root, processed, "X_test"), "X_test");
+        save(fullfile(root, processed, "y_test"), "y_test");
+    end
+    disp("Done!")
+    
+    % Fuzzy Inference System
+    disp("Model")
 end
 
 
-function preprocess(root, save)
-    % preprocess images and save them for the next time avoiding spending
-    % time in preprocessing
-    disp(save);
+function [data, labels] = preprocess(root, method)
+    % Load Data and Generate Labels
+    data = cell(1, 6);
+    labels = cell(1, 6);
+    
+    for label = 1 : 6
+        files = dir(fullfile(root, string(label), "*jpg"));
+        n_images = length(files);
+        data{label} = cell(1, n_images);
+        labels{label} = repmat(label, 1, n_images);
+        for n = 1 : n_images
+            [~, data{label}{n}] = createMask(imread(files(n).name));
+        end
+    end
+    
+    % Transform Data
+    data = transform(data, method);
+end
+
+function [X_train, y_train, X_test, y_test] = split_dataset(data, labels, ratio)
+    % Train Cell Arrays   
+    X_train = cell(1, 6);
+    y_train = cell(1, 6);
+    
+    % Test Cell Arrays
+    X_test = cell(1, 6);
+    y_test = cell(1, 6);
+    
+
+    for label = 1 : 6
+        % Dataset Length
+        n = length(labels{label});
+        
+        % Split Index
+        index = round(n * ratio);
+        
+        % Splitting
+        X_train{label} = data{label}(:, 1 : index);
+        y_train{label} = labels{label}(:, 1 : index);
+        
+        X_test{label} = data{label}(:, index + 1 : end);
+        y_test{label} = labels{label}(:, index + 1 : end);
+    end
+    
+    % Joining 
+    X_train = horzcat(X_train{:});
+    y_train = horzcat(y_train{:});
+    
+    X_test = horzcat(X_test{:});
+    y_test = horzcat(y_test{:});
 end
---
+++ TP3/src/transform.m
@@ -0,0 +1,33 @@
+function new_data = transform(data, method)
+    new_data = cell(1, 6);
+    
+    if method == "rgb_average"
+        n_features = 3;
+    else
+        n_features = 1;
+    end
+    
+    % Looping Through Images
+    for label = 1 : 6
+        n_images = length(data{label});
+        new_data{label} = zeros(n_features, n_images);
+        for n = 1 : n_images
+            img = data{label}{n};
+            new_data{label}(:, n) = eval(method + "(img)");
+        end
+    end
+end
+
+function values = rgb_average(image)
+    % Components
+    r = image(:, :, 1);
+    g = image(:, :, 2);
+    b = image(:, :, 3);
+    
+    % Components Mean (not counting background, which is black (0))
+    r = mean(r(r ~= 0)) / 255;
+    g = mean(g(g ~= 0)) / 255;
+    b = mean(b(b ~= 0)) / 255;
+    
+    values = [r; g; b];
+end
